cebf195ac30927bc465da8f3c60bdb1ad212b3dd
Pere Fernàndez <pere.fernandez@gmail.com>
2021-10-11 16:02:25 +0200

    dropdowns

--- formDropdowns.js
+++ formDropdowns.js
@@ -1,61 +1,3 @@
-function validateSalary() {
-    const salaryInput = document.getElementById('candidate.salary');
-    const salaryInputHelper = document.getElementById('candidate.salary_helper');
-
-    salaryInput.classList.remove('is-invalid');
-    salaryInputHelper.classList.remove('invalid-feedback');
-    salaryInputHelper.textContent = '';
-
-
-    const salary = salaryInput.value;
-
-    if (salary > 1000000) {
-        salaryInput.classList.add('is-invalid');
-        salaryInputHelper.classList.add('invalid-feedback');
-        salaryInputHelper.textContent = 'The salary is too high, employees cannot earn more than a 10000000$';
-        return false;
-    }
-    return true;
-}
-
-function validateSkills() {
-    const skillsInput = document.getElementById('candidate.skills');
-    const skillsInputHelper = document.getElementById('candidate.skills_helper');
-
-    skillsInput.classList.remove('is-invalid');
-    skillsInput.classList.remove('is-valid');
-    skillsInputHelper.classList.remove('invalid-feedback');
-    skillsInputHelper.classList.remove('valid-feedback');
-    skillsInputHelper.textContent = '';
-
-
-    const skills = skillsInput.value;
-
-    if(!skills) {
-        skillsInput.classList.add('is-invalid');
-        skillsInputHelper.classList.add('invalid-feedback');
-        skillsInputHelper.textContent = "Please fill the candidate skills!";
-        return false;
-    }
-
-    const skillsArray = skills.split(",");
-
-    if(skillsArray.length <5) {
-        skillsInput.classList.add('is-invalid');
-        skillsInputHelper.classList.add('invalid-feedback');
-        skillsInputHelper.textContent = "The candidate must have at least 5 valid skills!";
-        return false;
-    }
-
-    if(skillsArray.includes("Kogito")) {
-        skillsInput.classList.add('is-valid');
-        skillsInputHelper.classList.add('valid-feedback');
-        skillsInputHelper.textContent = "This guy is a Rockstar!";
-    }
-
-    return true;
-}
-
 function initSelect(select) {
     select.length = 0;
     select.value = "";
@@ -64,7 +6,6 @@ function initSelect(select) {
     defaultOption.textContent = "Choose option...";
     select.appendChild(defaultOption);
 }
-
 
 function fetchCountries() {
     const countrySelect = document.getElementById('candidate.address.country');
@@ -92,11 +33,10 @@ function fetchCountryStates() {
     const country = document.getElementById('candidate.address.country').value;
 
     const stateSelect = document.getElementById('candidate.address.state');
-    initSelect(stateSelect, "-- Select the State --");
-
+    initSelect(stateSelect);
 
     const citySelect = document.getElementById('candidate.address.city');
-    initSelect(citySelect, "-- Select the City --");
+    initSelect(citySelect);
 
     if (country) {
         const data = {
